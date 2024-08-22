5e698aee3864de12ff520c84b9b01a14ea10acfa
Jhonatha Martins <144477471+Jotacemartins@users.noreply.github.com>
2024-08-21 21:33:09 -0300

    First-Commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import { ThemeTogglerButton } from './components/theme-toggler-button/theme-toggler-button';
+import { ThemeProvider } from './contexts/theme-context';
+import { Card } from './components/card/card';
+
+function App() {
+  return (
+    <ThemeProvider>
+     <ThemeTogglerButton/>
+     <Card />
+    </ThemeProvider>
+  );
+}
+
+export default App;
